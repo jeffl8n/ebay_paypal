2132a75262fbad58f56898faded8ee24015190e8
onEnterFrame <this.onEnterFrame@gmail.com>
2015-04-26 16:46:12 -0400

    test file for azure

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Journal' });
 });
 
+/* GET home page. */
+router.get('/monkey', function(req, res, next) {
+  res.render('monkey');
+});
 
 
 module.exports = router;
